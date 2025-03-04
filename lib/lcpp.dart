40b2cd59276a4f9cb27dfbe46395dda4e79a6e1f
danemadsen <dane_madsen@hotmail.com>
2025-03-04 22:04:25 +1000

    refactor

--- lib/lcpp.dart
+++ lib/lcpp.dart
@@ -10,14 +10,14 @@ import 'dart:isolate';
 import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
 
-import 'src/bindings.dart';
+import 'src/utility/bindings.dart';
 
-part 'src/llama_isolated.dart';
-part 'src/llama_exception.dart';
-part 'src/llama.dart';
-part 'src/llama_native.dart';
+part 'src/llama/llama_isolated.dart';
+part 'src/llama/llama_exception.dart';
+part 'src/llama/llama.dart';
+part 'src/llama/llama_native.dart';
 part 'src/params/model_params.dart';
-part 'src/chat_message.dart';
+part 'src/utility/chat_message.dart';
 part 'src/params/isolate_params.dart';
 part 'src/params/context_params.dart';
 part 'src/params/sampling_params.dart';
