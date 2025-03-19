ac97b5483812a2ce86eda7bd3c3f5f91998055a4
danemadsen <dane_madsen@hotmail.com>
2025-03-19 10:36:44 +1000

    wasm

--- lib/src/web/bindings.dart
+++ lib/src/web/bindings.dart
@@ -1,14 +1,17 @@
+@JS()
+library llama_wasm;
+
 import 'dart:js_interop';
 
 // Wrap the global Module object’s _llama functions:
 @JS('Module._llama_default_params')
-external String llamaDefaultParams();
+external JSString llamaDefaultParams();
 
 @JS('Module._llama_llm_init')
-external int llamaInit(String params);
+external int llamaInit(JSString params);
 
 @JS('Module._llama_prompt_simple')
-external String llamaPrompt(String messages);
+external JSString llamaPrompt(JSString messages);
 
 @JS('Module._llama_llm_stop')
 external void llamaStop();
